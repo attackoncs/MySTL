66a4a018a97fafebe2e1c54cc45e625df2954125
attackoncs <2710651793@qq.com>
2022-04-29 11:03:24 +0800

    algorithm类

---
+++ MySTL/algorithm.h
@@ -0,0 +1,14 @@
+#ifndef MYSTL_ALGORITHM_H_
+#define MYSTL_ALGORITHM_H_
+
+#include "algo.h"
+#include "algobase.h"
+#include "heap_algo.h"
+#include "numeric.h"
+#include "set_algo.h"
+
+namespace mystl {
+    
+}
+
+#endif
